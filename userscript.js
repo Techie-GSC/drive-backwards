3e789539db28e545ccd1d84278577e02307715e0
Techie-GSC <65787765+Techie-GSC@users.noreply.github.com>
2023-11-10 13:28:58 -0600

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         MirrorDriveVid
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  MirrorVid
+// @author       You
+// @match        https://drive.google.com/file/*
+// @icon         https://t3.ftcdn.net/jpg/05/25/34/32/360_F_525343276_wzrufQ72rFbfxVTSJgUOHkjjUo0Td5ql.jpg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        document.addEventListener("click", function(){
+            document.getElementById("drive-viewer-video-player-object-0").style.transform = "scaleX(-1)";
+});
+}, false);
+
+})();
